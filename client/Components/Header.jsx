7ad29e5cb7edc918b4aa0ba62031bebb244d505a
halimahamood <halima@outlook.com>
2021-04-20 14:30:23 +0400

    Example meteor react stuff

---
+++ client/Components/Header.jsx
@@ -0,0 +1,16 @@
+Header = React.createClass({
+    render(){
+        return (
+            <div>
+                <div className="navbar navbar-default">
+                    <div className="container">
+                        <a className="navbar-brand">My Site Name</a>
+                    </div>
+
+                </div>
+            </div>
+
+        )
+
+    }
+})
---
+++ client/Components/Home/Post.jsx
@@ -0,0 +1,13 @@
+Post = React.createClass({
+    render() {
+        let {postId} = this.props;
+        console.log('post Id', postId);
+        return (
+            <div className="card">
+                <h3>{post.user}</h3>
+                <p>{post.content}</p>
+            </div>
+        )
+    }
+
+});
